dc5052a2ac3df06660ecd00d1a10ead9aab22718
dmitriy <master71512@mail.ru>
2022-12-23 22:26:54 +0300

    it works

--- 1.py
+++ 1.py
@@ -1,52 +1,31 @@
 from telegram import Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from random import randint
 
-# token = '5934547319:AAHs2zQ1MiBL1SdrApl-xwKnek_6xd219DU'
-bot = Bot(token='5934547319:AAHs2zQ1MiBL1SdrApl-xwKnek_6xd219DU')
-updater = Updater(token='5934547319:AAHs2zQ1MiBL1SdrApl-xwKnek_6xd219DU')
+bot = Bot(token='')
+updater = Updater(token='')
 dispatcher = updater.dispatcher
 
-a = 0
-b = 1
-# обучение бота
-
 def start(update, context):
-    context.bot.send_message(update.effective_chat.id, 'Хай! Сам чо как?')
-    return a
+    context.bot.send_message(update.effective_chat.id, 'Привет!\n\nЯ умею удалять из твоих сообщений слова, в которых есть сочетание "абв"')
+    context.bot.send_message(update.effective_chat.id, 'Напиши мне любое сообщение, и я верну его обратно без слов с "абв"')
 
-def rand(update, context):
-    context.bot.send_message(update.effective_chat.id, randint(1, 100))
-
-def func(update, context):
+def del_abv(update, context):
     text = update.message.text
-    if 'прив' in text.lower():
-        context.bot.send_message(update.effective_chat.id, 'Салам пополам!')
-    else:
-        context.bot.send_message(update.effective_chat.id, 'Здороваться не учили??')
-
-def how_are_you(update, context):
-    text = update.message.text
-    if 'норм' or 'хор' in text.lower():
-        context.bot.send_message(update.effective_chat.id, 'Каеф ))')
-    else:
-        context.bot.send_message(update.effective_chat.id, 'Вот жаль ...')
-    context.bot.send_message(update.effective_chat.id, 'Как погодка?')
-
-
-
+    user_message = text.split()
+    i = 0
+    while i < len(user_message):
+        if 'абв' in user_message[i].lower():
+            del user_message[i]
+            i -= 1
+        i += 1
+    user_message = ' '.join(user_message)
+    context.bot.send_message(update.effective_chat.id, user_message)
+    
 start_handler = CommandHandler('start', start)
-random_handler = CommandHandler('random', rand)
-message_handler = MessageHandler(Filters.text, func)
-message_handler = MessageHandler(Filters.text, how_are_you)
-# message_handler = MessageHandler(Filters.text, func)
-# message_handler = MessageHandler(Filters.text, func)
-# message_handler = MessageHandler(Filters.text, func)
+message_handler = MessageHandler(Filters.text, del_abv)
 
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(random_handler)
 dispatcher.add_handler(message_handler)
 
-#запуск и закрытие бота
 updater.start_polling()
 updater.idle()
